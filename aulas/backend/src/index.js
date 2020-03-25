f22c85bc91a0cd620ea052a34f7cfed7e84f9433
douglas <dougfeltrim@gmail.com>
2020-03-24 23:55:18 -0300

    feitas as migrações iniciais, criando as tabelas

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+
+app.use(express.json()); 
+app.use(routes);
+/**
+ * Metodos HTTP:
+ *GET: Buscar/listar uma informação do back-end
+ *POST: Criar uma informação no back-end
+ *PUT: Alterar uma informação no back-end
+ *Delete: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parametros:
+  * Query:parametros nomeados na rota apos "?" filtros,paginação...
+  * Route Params: parametros utilizados para identificar recursos
+  * Request body:  corpo da requisicao
+  * 
+  * 
+  * 
+  * 
+  */
+
+
+
+app.listen(3333); 
